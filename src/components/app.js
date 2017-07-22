d99a4e7ef8336500256b375654fc97d6e1eb9ce1
Hack Reactor Student <student@hackreactor.com>
2017-07-22 12:35:39 -0700

    search works!

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,23 @@
 angular.module('video-player')
 .component('app', {
 
-  controller: function() { 
+  controller: function(youTube) { 
     this.videos = exampleVideoData;
     this.currentVideo = exampleVideoData[0];
     this.selectVideo = (video) => { 
       this.currentVideo = video;
     };
-    this.searchResults = function() {
+    this.searchResults = (searchstring) => {
+      //console.log('before search: ', searchstring);
+      youTube.search((response) => { 
+        this.videos = response.data.items; 
+        this.currentVideo = response.data.items[0];
+      }, searchstring);
+      //console.log('succes :', this.videos);// calling the service
+      
     };
     
   },
-
-  
 
 
   // code that will render this component to the DOM
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,7 +1,77 @@
 angular.module('video-player')
-.service('youTube', function() {
+.service('youTube', function($http) {
   
-  this.search = function() { 
-    
+  this.search = function (callbackFun, query) { 
+    //console.log('in search: ', query);
+    // Simple GET request example:
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        key: 'AIzaSyCsng51JWl4hSarHOd1g-h_6rvir3VhkBU',
+        part: 'snippet',
+        q: query,
+        maxResults: '5',
+        videoEmbeddable: 'true',
+        type: 'video',
+        datatype: 'json',
+      }
+    }).then(function successCallback(response) {
+      callbackFun(response);
+      //console.log('success: ', response);
+        // this callback will be called asynchronously
+        // when the response is available
+    }, function errorCallback(response) {
+      //console.log('error: ', response);
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+        
   };
 });
+
+
+
+// var searchYouTube = (options, callback) => {
+//   // return exampleVideoData;
+//   // callback must be the event handler
+//   $.ajax({
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     type: 'GET',
+//     data: {
+//       key: options.key,
+//       part: 'snippet',
+//       q: options.query,
+//       maxResults: options.max,
+//       videoEmbeddable: 'true',
+//       type: 'video'
+//     },
+//     datatype: 'json',
+    
+//     success: (data) => {
+//       // console.log(data);
+//       callback(data.items);
+//     },
+//     error: (error) => {
+//       console.error(error.responseText);
+//     }
+//   });
+// };
+
+
+
+//angular.element(document).injector().get('service.youTube');
+
+// var $inj = angular.injector(['video-player']);
+// var serv = $inj.get('youTube');
+// serv.search();
+
+//var t = angular.element(document.querySelector('body')).injector().get('youTube');
+//t.myServiceMethod().then(function(response) { console.log(response); });
+
+
+// The $http provider should receive a params object with the following properties:
+// query - the string to search for
+// maxResults - the maximum number of videos to get, which should default to 5
+// key - an authorized YouTube Browser API key
+// Only fetch embeddable videos
